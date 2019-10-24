259c9532f6f9ab6c2619a3f8d1e5fdb0ff7164ad
valaEZD <andres0@outlook.com>
2019-10-23 21:01:41 -0500

    add service Alumno

--- src/app/pages/inicio/inicio.page.ts
+++ src/app/pages/inicio/inicio.page.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { MenuController } from '@ionic/angular';
 
+import { AlumnoService } from '../../services/alumno.service';
+
 @Component({
   selector: 'app-inicio',
   templateUrl: './inicio.page.html',
@@ -9,8 +11,10 @@ import { MenuController } from '@ionic/angular';
 export class InicioPage implements OnInit {
 
   activeMenu: string;
+  codigo: string;
+  nip: string;
 
-  constructor(public menu: MenuController) {
+  constructor(public menu: MenuController, public alumnoService: AlumnoService) {
     this.menuActive();
    }
   //funcion para inhabilitar menu en una pagina false=no mostrar true=mostrar menu
@@ -20,6 +24,19 @@ export class InicioPage implements OnInit {
    }
 
   ngOnInit() {
+    this.alumnoService.getTopHeadlines()
+    .subscribe( resp => {
+        console.log('Alumno', resp);
+    });
+  }
+  
+  validar() {
+    console.log('Prueba', this.codigo, ' ', this.nip);
+    
+    this.alumnoService.validarAlumno( this.codigo, this.nip )
+    .subscribe( resp => {
+        console.log(resp);
+    });
   }
 
 }
---
+++ src/app/services/alumno.service.ts
@@ -0,0 +1,43 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpParams } from '@angular/common/http';
+//import { Top } from '../interfaces/interfaces';
+
+import { apiKey } from "../../app/apis/servidor.js";
+
+
+var headers = new Headers();
+    headers.append("Accept", 'application/json');
+    headers.append('Content-Type', 'application/json' );
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AlumnoService {
+
+  constructor( private http: HttpClient ) { }
+  
+  getTopHeadlines() {
+    //const requestOptions = new RequestOptions({ headers: headers }); 
+    
+    let httpParams = new HttpParams()
+                        .set('codigo', '212099002')
+	                    .set('nip', '1234');
+                        //.set('year', year);
+  
+    //return this.http.get<Top>('http://localhost/Horario/getAlumnos.php', { params: httpParams } );
+    //return this.http.get<Top>(apiKey+'getAlumno.php', { params: httpParams } );
+    
+    return this.http.get(apiKey+'getAlumnoValido.php', { params: httpParams } );
+    
+  }
+  
+  validarAlumno( codigo: string, nip: string ) {
+    let httpParams = new HttpParams()
+                        .set('codigo', codigo )
+	                    .set('nip', nip );
+    
+    return this.http.get(apiKey+'getAlumnoValido.php', { params: httpParams } );
+    
+  }
+  
+}
