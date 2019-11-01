6267784770f7783e4d5b90746da00f516b9473f7
Daniel <jd_ft_b.snk@hotmail.com>
2019-10-31 20:55:40 -0600

    arreglo

--- src/app/apis/servidor.js
+++ src/app/apis/servidor.js
@@ -1,6 +1,1 @@
-<<<<<<< HEAD
 export const apiKey = 'http://167.71.150.242/Scripts/'
-=======
-export const apiKey = 'http://localhost/Horario/'
-//export const apiKey = 'http://167.71.150.242/Scripts/'
->>>>>>> 86ebef08b1aa8c7532b0ce854208e6ac64410883
