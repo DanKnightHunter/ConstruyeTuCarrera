86ebef08b1aa8c7532b0ce854208e6ac64410883
valaEZD <andres0@outlook.com>
2019-10-31 20:15:30 -0600

    Pagina para generar horario

---
+++ src/app/pages/generar-horario/generar-horario.page.ts
@@ -0,0 +1,42 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-generar-horario',
+  templateUrl: './generar-horario.page.html',
+  styleUrls: ['./generar-horario.page.scss'],
+})
+export class GenerarHorarioPage implements OnInit {
+
+
+  tiempo = [];
+  hora = [];
+  
+
+  constructor() { }
+
+  ngOnInit() {
+    for(var i = 5, j = 1; i < 135; i+=5, j++)    {
+        
+        this.tiempo.push(i);
+        if((j%11)==0)    {
+            i+=40;
+        }
+    }
+  
+  }
+    
+    opcionesTurno: any = {
+        header: 'Turno',
+        subHeader: 'Selecciona el turno',
+        message: 'El horario se creara en el turno selecionado',
+        translucent: true
+    };
+    
+    opcionesDia: any = {
+        header: 'Dias',
+        subHeader: 'Selecciona el o los dias',
+        message: 'El horario se creara en los dias selecionados',
+        translucent: true,
+    };
+
+}
